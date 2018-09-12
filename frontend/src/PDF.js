fca13450179dc9f83de04a366a66a46227adb1d0
SDelaney1234 <sebastien.delaney@hotmail.com>
2018-09-12 15:50:54 +0100

    modalworks

--- frontend/src/PDF.js
+++ frontend/src/PDF.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf';
-import AddModal from "./Add";
+import {Modal,Button} from 'reactstrap'
 
 class PDF extends Component {
-    state = {
-        numPages: 1,
-        pageNumber: 1,
+    constructor(){
+        super();
+        this.state = {
+            modal: false,
+            numPages: 1,
+            pageNumber: 1
+        };
+        this.toggle = this.toggle.bind(this);
     }
+
 
     onDocumentLoad = ({ numPages }) => {
         this.setState({ numPages });
+    }
+
+    toggle(){
+        this.setState({
+            modal: !this.state.modal
+        });
     }
 
     render() {
         const { pageNumber, numPages } = this.state;
 
         return (
-            <Modal>
+            <div>
+            <Button color="primary" onClick={this.toggle}>PDF</Button>
+            <Modal isOpen={this.state.modal} toggle={this.toggle}>
             <div>
                 <Document
                     file="pdf-sample.pdf"
@@ -26,6 +40,7 @@ class PDF extends Component {
                 <p>Page {pageNumber} of {numPages}</p>
             </div>
             </Modal>
+            </div>
         );
     }
 }
