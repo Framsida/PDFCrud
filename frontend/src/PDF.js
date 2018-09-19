33d129f19e0cb63fdabc7095ad965baebf9c6bb1
SDelaney <rekha.vaghela@hotmail.co.uk>
2018-09-19 15:12:48 +0100

    Jack - got the next and prev to work

--- frontend/src/PDF.js
+++ frontend/src/PDF.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf';
-import {Modal,Button} from 'reactstrap'
+import {NavLink, Nav, Modal,Button} from 'reactstrap'
 
 class PDF extends Component {
     constructor(props){
@@ -24,22 +24,51 @@ class PDF extends Component {
             modal: !this.state.modal
         });
     }
+    handlePrevious = () => {
+        this.setState({ pageNumber: this.state.pageNumber - 1 });
+        this.setState({ pageNumber: this.state.pageNumber - 1 });
+    }
 
+    handleNext = () => {
+        this.setState({ pageNumber: this.state.pageNumber + 1 });
+    }
 
-
-    render() {
-        const { pageNumber, numPages } = this.state;
-
+    renderPagination = (pageNumber, numPages) => {
+        let previousButton = <li className="previous" onClick={this.handlePrevious}><a><i className="fa fa-arrow-left"></i> Previous</a></li>;
+        if (pageNumber === 1) {
+            previousButton = <li className="previous disabled"><a><i className="fa fa-arrow-left"></i> Previous</a></li>;
+        }
+        let nextButton = <li className="next" onClick={this.handleNext}><a>Next <i className="fa fa-arrow-right"></i></a></li>;
+        if (pageNumber === numPages) {
+            nextButton = <li className="next disabled"><a>Next <i className="fa fa-arrow-right"></i></a></li>;
+        }
         return (
-            <div>
-            <Button color="primary" onClick={this.toggle}>PDF</Button>
-            <Modal isOpen={this.state.modal} toggle={this.toggle}>
-            <div>
+            <Nav color="light">
+                <NavLink><button class="btn btn-danger">{previousButton}</button></NavLink>
+                <NavLink> <button class="btn btn-success"> {nextButton}</button></NavLink>
+            </Nav>
+        );
+    }
+
+                render() {
+                let pagination = null;
+                if (this.state.numPages) {
+                pagination = this.renderPagination(this.state.pageNumber, this.state.numPages);
+            }
+                const { pageNumber, numPages } = this.state;
+
+                return (
+
+                <div>
+                <Button color="primary" onClick={this.toggle}>PDF</Button>
+                <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                <div>
                 <Document
-                    file={`http://localhost:3002/api/viewFileWithID/${this.id}`}
-                    onLoadSuccess={this.onDocumentLoad}>
-                    <Page pageNumber={pageNumber} />
+                file={`http://localhost:3002/api/viewFileWithID/${this.id}`}
+                onLoadSuccess={this.onDocumentLoad}>
+                <Page pageNumber={pageNumber} />
                 </Document>
+                {pagination}
                 <p>Page {pageNumber} of {numPages}</p>
             </div>
             </Modal>
